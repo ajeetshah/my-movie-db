13a5c0b49610a970b6d0350a0b0e1dab16149227
code-star <code69023165238739654@star.com>
2021-04-07 00:32:33 +0530

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,13 @@ const TrendingMovies = () => {
       });
   }, []);
 
-  const moviesIdCollections = trending.map((item) => item.id);
-
   return (
     <div className="trending-container">
       <h1 className="row-label" tabIndex={0}>
         This Weeks Trending Tittles
       </h1>
-      {moviesIdCollections.map((elm) => (
-        <MovieDetail key={elm} id={elm} trending={trending} />
+      {trending.map((item) => (
+        <MovieDetail key={item.id} movieItem={item} />
       ))}
     </div>
   );
@@ -36,34 +34,34 @@ const getMovieDetail = (movieId) => {
   return fetch(url);
 };
 
-function MovieDetail({ id, trending }) {
+function MovieDetail({ movieItem }) {
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
-    getMovieDetail(id)
+    getMovieDetail(movieItem.id)
       .then((res) => res.json())
-      .then((res) => setMovie(res?.results?.US?.link));
-  }, [id]);
+      .then((res) => {
+        setMovie(res?.results?.US?.link);
+      });
+  }, [movieItem.id]);
 
   return (
     <ul className="flexed-trending">
-      {trending.map((it) => (
-        <div className="poster-container" key={it.id}>
-          <a target="_blank" rel="noopener noreferrer" href={movie}>
-            <li className="list-item">
-              <img
-                className="image-element"
-                tabIndex={0}
-                aria-label={it.title}
-                title={`--Title: ${it.title}--  --Description:    ${it.overview}--  --Vote Average: ${it.vote_average}`}
-                alt="movie poster"
-                src={`https://image.tmdb.org/t/p/w500${it.poster_path}`}
-              />
-              <h3 className="posterTitle">{it.title}</h3>
-            </li>
-          </a>
-        </div>
-      ))}
+      <div className="poster-container">
+        <a target="_blank" rel="noopener noreferrer" href={movie}>
+          <li className="list-item">
+            <img
+              className="image-element"
+              tabIndex={0}
+              aria-label={movieItem.title}
+              title={`--Title: ${movieItem.title}--  --Description:    ${movieItem.overview}--  --Vote Average: ${movieItem.vote_average}`}
+              alt="movie poster"
+              src={`https://image.tmdb.org/t/p/w500${movieItem.poster_path}`}
+            />
+            <h3 className="posterTitle">{movieItem.title}</h3>
+          </li>
+        </a>
+      </div>
     </ul>
   );
 }
