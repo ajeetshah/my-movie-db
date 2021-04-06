91a6336936347aa74a72f5137aed01ef9a0da82d
code-star <code69023165238739654@star.com>
2021-04-07 00:08:36 +0530

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const key = "PUT_YOUR_KEY_HERE_DONT_SHOW_IT_TO_OTHERS";
+
+const TrendingMovies = () => {
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    const trendUrl = `https://api.themoviedb.org/3/trending/movie/week?api_key=${key}`;
+    fetch(trendUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setTrending(data.results);
+      })
+      .catch((error) => {
+        console.log("Error!! Data interupted!:", error);
+      });
+  }, []);
+
+  const moviesIdCollections = trending.map((item) => item.id);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trending-container">
+      <h1 className="row-label" tabIndex={0}>
+        This Weeks Trending Tittles
+      </h1>
+      {moviesIdCollections.map((elm) => (
+        <MovieDetail key={elm} id={elm} trending={trending} />
+      ))}
     </div>
+  );
+};
+
+const getMovieDetail = (movieId) => {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=${key}`;
+  return fetch(url);
+};
+
+function MovieDetail({ id, trending }) {
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    getMovieDetail(id)
+      .then((res) => res.json())
+      .then((res) => setMovie(res?.results?.US?.link));
+  }, [id]);
+
+  return (
+    <ul className="flexed-trending">
+      {trending.map((it) => (
+        <div className="poster-container" key={it.id}>
+          <a target="_blank" rel="noopener noreferrer" href={movie}>
+            <li className="list-item">
+              <img
+                className="image-element"
+                tabIndex={0}
+                aria-label={it.title}
+                title={`--Title: ${it.title}--  --Description:    ${it.overview}--  --Vote Average: ${it.vote_average}`}
+                alt="movie poster"
+                src={`https://image.tmdb.org/t/p/w500${it.poster_path}`}
+              />
+              <h3 className="posterTitle">{it.title}</h3>
+            </li>
+          </a>
+        </div>
+      ))}
+    </ul>
   );
 }
 
-export default App;
+export default TrendingMovies;
